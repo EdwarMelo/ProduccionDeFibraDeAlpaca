344fb518acd859d7b008bc33a81583aea3fa994d
EdwarMeloH <edwarmelo16@gmail.com>
2026-01-07 16:48:15 -0500

    Se prepara para la carga de datos

--- produccion_script.R
+++ produccion_script.R
@@ -1,4 +1,7 @@
 ################################################################################
 # Produccion de Fibra de Alpaca y sus Factores Productivos en un Escenario de 
 # Cambio Climatico, periodo 1992-2022
+
 ################################################################################
+# Se carga los datos de series temporales
+
