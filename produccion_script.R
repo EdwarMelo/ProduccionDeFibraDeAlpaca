1a99476bd1484a2f4b43294bbb012b1138f9e299
EdwarMeloH <edwarmelo16@gmail.com>
2026-01-07 16:40:12 -0500

    Este es el primer commit de la Investigacion

---
+++ produccion_script.R
@@ -0,0 +1,4 @@
+################################################################################
+# Produccion de Fibra de Alpaca y sus Factores Productivos en un Escenario de 
+# Cambio Climatico, periodo 1992-2022
+################################################################################
