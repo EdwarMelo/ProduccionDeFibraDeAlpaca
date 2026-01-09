e57b79b098d4080f61a43cdbf54a8284f032a5f5
EdwarMeloH <edwarmelo16@gmail.com>
2026-01-08 23:40:45 -0500

    conversion del dataset a texto plano (de .xlsx a .csv)

--- produccion_script.R
+++ produccion_script.R
@@ -1,18 +1,42 @@
 ################################################################################
 # Produccion de Fibra de Alpaca y sus Factores Productivos en un Escenario de 
 # Cambio Climatico, periodo 1992-2022
-
 ################################################################################
 
-#####
-# Transformacion de Datos de formato .xlsx  a .csv 
+#### Configuraciones Iniciales ####
+# Se limpia memoria
+rm(list = ls())
+# Situar el directorio de trabajo 
+setwd("D:/TESIS/git/backup_git") # flexible a modificacion
+# Verificacion de ubicacion del directorio de trabajo
 getwd()
-setwd("D:/TESIS/git/backup_git")
+# Verificacion del contenido de directorio de trabajo
+dir()
+
+#### Se carga el dataset desde el repositorio de GitHub ####
+dataset 
+
+
+# Transformacion de Datos de formato .xlsx  a .csv 
+
+
 
 library(readxl)
 data <- read_excel("data_ts.xlsx")
 write.csv(data, file="data_ts.csv")
 dataset <- read.csv("data_ts.csv")
+
+
+colnames(data)
+dataset <- keep(dataset(colnames(data)))
+
+dataset$X <- NULL
+write.csv(dataset$X <- NULL, file="data_ts.csv")
+dataset <- read.csv("data_ts.csv")
+
+
+
+
 
 # Se carga los datos de series temporales
 
