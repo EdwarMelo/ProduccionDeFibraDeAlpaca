c64c8ffb335abb8ea6aa211b9e36a3a9b360dd4b
EdwarMeloH <edwarmelo16@gmail.com>
2026-01-08 19:08:33 -0500

    conversion de la data a texto plano

--- produccion_script.R
+++ produccion_script.R
@@ -6,10 +6,14 @@
 
 #####
 # Transformacion de Datos de formato .xlsx  a .csv 
+getwd()
+setwd("D:/TESIS/git/backup_git")
 
 
+data <- read.csv("data_ts.xlsx")
+write.csv(data, file="data_ts.csv")
 
-
+help(write.csv)
 
 # Se carga los datos de series temporales
 
